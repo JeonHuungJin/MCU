181741d2080e5bf0d687c208562c6a3b2c9c547b
JeonHyungJin <50432602+JeonHuungJin@users.noreply.github.com>
2022-05-26 00:23:26 +0900

    Update UART.c

--- AVR/ATmega128/UART/UART.c
+++ AVR/ATmega128/UART/UART.c
@@ -11,11 +11,11 @@
 #include <stdio.h>
 #include "UART.h"
 
-void uart_init(uint8_t uart_num, uint8_t isr, uint8_t u2x ,uint16_t burr){
+void uart_init(uint8_t uart_num, uint8_t rx_isr, uint8_t tx_isr, uint8_t u2x ,uint16_t burr){
 	if(uart_num == UART0){
-		UCSR0A = u2x<<U2X0;						// 2배속 모드 설정
+		UCSR0A = u2x<<U2X0;				// 2배속 모드 설정
 		UCSR0B = 1<<RXEN0 | 1<<TXEN0;			// 수신, 송신 가능
-		UCSR0B |= isr<<RXCIE0 | isr<<TXCIE0;	// 송, 수신 인터럽트 설정
+		UCSR0B |= rx_isr<<RXCIE0 | tx_isr<<TXCIE0;	// 송, 수신 인터럽트 설정
 		
 		UBRR0H = burr>>8;	// 보오레이트 상위 레지스터
 		UBRR0L = burr&0xff;	// 보오레이트 하위 레지스터
@@ -25,9 +25,9 @@ void uart_init(uint8_t uart_num, uint8_t isr, uint8_t u2x ,uint16_t burr){
 		#endif
 	}
 	else if(uart_num == UART1){
-		UCSR1A = u2x<<U2X1;						// 2배속 모드 설정
+		UCSR1A = u2x<<U2X1;				// 2배속 모드 설정
 		UCSR1B = 1<<RXEN1 | 1<<TXEN1;			// 수신, 송신 가능
-		UCSR1B |= isr<<RXCIE1 | isr<<TXCIE1;	// 송, 수신 인터럽트 설정
+		UCSR1B |= rx_isr<<RXCIE1 | tx_isr<<TXCIE1;	// 송, 수신 인터럽트 설정
 				
 		UBRR1H = burr>>8;	// 보오레이트 상위 레지스터
 		UBRR1L = burr&0xff;	// 보오레이트 하위 레지스터
@@ -61,11 +61,11 @@ void uart0_NewLine(){
 
 uint8_t uart1_rx(FILE *stream){
 	while(!(UCSR1A & (1<<RXC1)));	// 수신 대기
-	return UDR1;					// 수신된 값 반환
+	return UDR1;			// 수신된 값 반환
 }
 void uart1_tx(uint8_t data, FILE *stream){
 	while(!(UCSR1A & (1<<UDRE1)));	// 송신 준비
-	UDR1 = data;					// UDR0에 data 값을 대입함으로 데이터 전송 완료
+	UDR1 = data;			// UDR0에 data 값을 대입함으로 데이터 전송 완료
 }
 void uart1_tx_str(char *text){
 	while(*text != '\0'){
